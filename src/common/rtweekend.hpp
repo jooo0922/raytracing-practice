5070ab59af2b8443fc47d7c49e6e0ca3e46908e3
jooo0922 <jooo0922@gmail.com>
2025-03-04 00:59:10 +0900

    feat: define common header

---
+++ src/common/rtweekend.hpp
@@ -0,0 +1,27 @@
+#ifndef RTWEEKEND_HPP
+#define RTWEEKEND_HPP
+
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <memory>
+#include <cstdio>
+#include <string>
+#include <fstream>
+
+// constants
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+// utils
+inline double degrees_to_radians(double degrees)
+{
+  return degrees * pi / 180.0f;
+};
+
+// common
+#include "color.hpp"
+#include "ray.hpp"
+#include "vec3.hpp"
+
+#endif /* RTWEEKEND_HPP */
