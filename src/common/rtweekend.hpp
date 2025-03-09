fed55e50c2b829fd20393afbefa1e1860096476a
jooo0922 <jooo0922@gmail.com>
2025-03-09 13:28:11 +0900

    feat: implement random number generating util func

--- src/common/rtweekend.hpp
+++ src/common/rtweekend.hpp
@@ -2,6 +2,7 @@
 #define RTWEEKEND_HPP
 
 #include <cmath>
+#include <cstdlib>
 #include <iostream>
 #include <limits>
 #include <memory>
@@ -17,6 +18,18 @@ const double pi = 3.1415926535897932385;
 inline double degrees_to_radians(double degrees)
 {
   return degrees * pi / 180.0f;
+};
+
+inline double random_double()
+{
+  // 0.0f <= n < 1.0f 사이의 난수 생성
+  return std::rand() / (RAND_MAX + 1.0f);
+};
+
+inline double random_double(double min, double max)
+{
+  // min <= n < max 사이의 난수 생성
+  return min + (max - min) * random_double();
 };
 
 // common
