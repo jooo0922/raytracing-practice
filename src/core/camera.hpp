f3d82c05048e4b2682bf785f13e53b80a381f56c
jooo0922 <jooo0922@gmail.com>
2025-04-13 14:17:52 +0900

    feat: calculate camera params using lookat and lookfrom

--- src/core/camera.hpp
+++ src/core/camera.hpp
@@ -83,12 +83,12 @@ private:
      * -> fov 가 커질수록 카메라가 담을 수 있는 시야각이 넓어지므로,
      * 마치 zoom-out 되는 듯한 효과를 줄 수 있음.
      */
-    auto focal_length = 1.0;                                                                   // 카메라 중점(eye point)과 viewport 사이의 거리 (현재는 단위 거리 1로 지정함.)
+    auto focal_length = (lookfrom - lookat).length();                                          // 카메라 중점(eye point)과 viewport 사이의 거리 -> view direction(lookfrom -> lookat)의 거리로 정의
     auto theta = degrees_to_radians(vfov);                                                     // 카메라 수직 방향 fov 각도 단위를 degree -> radian 으로 변환
     auto h = std::tan(theta / 2);                                                              // 카메라 수직 방향 fov 절반 지점 방향을 밑변으로 하는 직각삼각형의 tan 비 계산
     auto viewport_height = 2 * h * focal_length;                                               // viewport 높이 정의 (직각삼각형의 밑변의 길이 * tan 비 = 직각삼각형의 높이 계산(= viewport 높이 절반) -> 여기에 2배를 곱해서 viewport 최종 높이 계산)
     auto viewport_width = viewport_height * (static_cast<double>(image_width) / image_height); // viewport 너비 정의 (기존 aspect_ratio 는 casting 에 의해 소수점이 잘려나간 image_width & image_height 의 종횡비와 다르므로, 실제 image_width & image_height 로 종횡비 재계산).
-    camera_center = point3(0, 0, 0);                                                           // 3D Scene 상에서 카메라 중점(eye point) > viewport 로 casting 되는 모든 ray 의 출발점이기도 함.
+    camera_center = lookfrom;                                                                  // 3D Scene 상에서 카메라 중점(eye point) -> 카메라 위치(= view direction 출발점)으로 정의
 
     /**
      * viewport 구조에 존재하는 벡터 및 정점들 정의
