e84f7f8a8fca84948b02050d44e68ea71149a866
jooo0922 <jooo0922@gmail.com>
2025-04-20 01:14:29 +0900

    feat: adjust samples per pixel for reasonable runtime

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
   hittable_list world;
 
   /** 각 Hittable 객체에 적용할 재질(Material)을 shared_ptr로 생성하여 공유 가능하도록 관리 */
-  auto ground_material = std::make_shared<lambertian>(color(0.8f, 0.8f, 0.0f));
+  auto ground_material = std::make_shared<lambertian>(color(0.5f, 0.5f, 0.5f));
   world.add(std::make_shared<sphere>(point3(0.0f, -1000.0f, -1.0f), 1000.0f, ground_material)); // 반지름이 1000 인 지면 sphere 추가
 
   /** 484개(= 22 * 22)의 소형 sphere 생성 후 world 에 추가 */
@@ -88,8 +88,8 @@ int main(int argc, char *argv[])
   // 주요 이미지 파라미터 설정
   cam.image_width = 1200;
   cam.aspect_ratio = 16.0f / 9.0f;
-  cam.samples_per_pixel = 500;
-  cam.max_depth = 50;
+  cam.samples_per_pixel = 10;
+  cam.max_depth = 20;
 
   // camera transform 관련 파라미터 설정
   cam.vfov = 20.0f;
