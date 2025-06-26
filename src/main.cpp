a506275de75c5a4b9cb3159a7ed52453c10fa332
jooo0922 <jooo0922@gmail.com>
2025-06-26 23:52:56 +0900

    feat: implement light scene rendering func

--- src/main.cpp
+++ src/main.cpp
@@ -178,7 +178,7 @@ void perlin_sphere(std::ofstream &output_file)
 
   // perlin noise 적용을 위해 noise_texture 생성 후 lambertian material 에 적용
   auto pertext = std::make_shared<noise_texture>(4);
-  // 반지름이 각각 2, 1000 인 두 checkered sphere 추가
+  // 반지름이 각각 2, 1000 인 두 sphere 추가
   world.add(std::make_shared<sphere>(point3(0.0f, -1000.0f, 0.0f), 1000.0f, std::make_shared<lambertian>(pertext)));
   world.add(std::make_shared<sphere>(point3(0.0f, 2.0f, 0.0f), 2.0f, std::make_shared<lambertian>(pertext)));
 
@@ -250,6 +250,51 @@ void quads(std::ofstream &output_file)
   cam.render(output_file, world);
 };
 
+// light scene 렌더링 함수
+void simple_light(std::ofstream &output_file)
+{
+  /** world(scene) 역할을 수행하는 hittable_list 생성 및 hittable object 추가 */
+  hittable_list world;
+
+  // perlin noise 적용을 위해 noise_texture 생성 후 lambertian material 에 적용
+  auto pertext = std::make_shared<noise_texture>(4);
+  // 반지름이 각각 2, 1000 인 두 sphere 추가
+  world.add(std::make_shared<sphere>(point3(0.0f, -1000.0f, 0.0f), 1000.0f, std::make_shared<lambertian>(pertext)));
+  world.add(std::make_shared<sphere>(point3(0.0f, 2.0f, 0.0f), 2.0f, std::make_shared<lambertian>(pertext)));
+
+  /**
+   * 광원 객체에 적용할 재질(diffuse_light)을 shared_ptr로 생성
+   * -> 고강도 광원 색상값(color(4.0f, 4.0f, 4.0f) 사용 목적 관련 하단 필기 참고
+   */
+  auto difflight = std::make_shared<diffuse_light>(color(4.0f, 4.0f, 4.0f));
+
+  // 광원으로 사용할 quad 생성 후 world 에 추가
+  world.add(std::make_shared<quad>(point3(3.0f, 1.0f, -2.0f), vec3(2.0f, 0.0f, 0.0f), vec3(0.0f, 2.0f, 0.0f), difflight));
+
+  /** camera 객체 생성 및 이미지 렌더링 수행 */
+  camera cam;
+
+  // 주요 이미지 파라미터 설정
+  cam.image_width = 400;
+  cam.aspect_ratio = 16.0f / 9.0f;
+  cam.samples_per_pixel = 100;
+  cam.max_depth = 50;
+  // ray 와 충돌한 물체가 없을 경우 반환할 scene 배경색(solid color. no gradient) 정의
+  cam.background = color(0.0f, 0.0f, 0.0f);
+
+  // camera transform 관련 파라미터 설정
+  cam.vfov = 20.0f;
+  cam.lookfrom = point3(26.0f, 3.0f, 6.0f);
+  cam.lookat = point3(0.0f, 2.0f, 0.0f);
+  cam.vup = vec3(0.0f, 1.0f, 0.0f);
+
+  // defocus blur 관련 파라미터 성정
+  cam.defocus_angle = 0.0f;
+
+  // 카메라 및 viewport 파라미터 내부에서 자동 초기화 후 .ppm 이미지 렌더링
+  cam.render(output_file, world);
+};
+
 int main(int argc, char *argv[])
 {
   /** 명령줄 인수로 출력 파일(= .ppm 이미지 파일) 경로 전달받기 */
@@ -271,7 +316,7 @@ int main(int argc, char *argv[])
   }
 
   // switch 문으로 렌더링을 원하는 장면 선택 가능
-  switch (5)
+  switch (6)
   {
   case 1:
     bouncing_spheres(output_file);
@@ -287,6 +332,9 @@ int main(int argc, char *argv[])
     break;
   case 5:
     quads(output_file);
+    break;
+  case 6:
+    simple_light(output_file);
     break;
   }
 
@@ -334,4 +382,24 @@ int main(int argc, char *argv[])
  *
  * 또한, 유리 구슬 두께를 고려하여 inner sphere 의 반지름을 outer sphere 반지름보다
  * 유리 구슬 두께만큼 작게 설정해야 할 것임.
+ */
+
+/**
+ * 고강도 광원 색상값 (ex> color(4.0f, 4.0f, 4.0f))을 사용하는 이유
+ *
+ * ray tracing에서 조명을 시뮬레이션할 때, 광원의 색상 값은 단순한 색 표현을 넘어
+ * 빛의 '강도(intensity)'를 의미합니다. 예를 들어 color(4.0f, 4.0f, 4.0f)는 흰색 광원이지만,
+ * 일반적인 color(1.0f, 1.0f, 1.0f)보다 4배 더 밝은 빛을 방출합니다.
+ *
+ * 내부적으로 광선(ray)이 장면을 따라 반사되거나 산란될 때마다 감쇠(attenuation)가 발생합니다.
+ * 이때, 광원의 출발 강도가 충분히 높다면 감쇠가 누적되더라도 여전히
+ * 최종적으로 1.0에 가까운 값이 유지될 수 있습니다.
+ *
+ * 최종 이미지 버퍼에 픽셀 값을 기록할 때는 write_color() 함수에서 각 색상 성분이
+ * [0.0, 1.0] 범위로 clamp되므로, 계산 결과가 1.0보다 크더라도 출력은 1.0에 맞춰집니다.
+ * 하지만 **1.0에 가까운 값이 많을수록 더 밝게 보이며**, 충분히 강한 광원 색상은
+ * 간접 조명이나 다중 반사를 거친 후에도 시각적으로 더 밝은 결과를 만들어냅니다.
+ *
+ * 따라서, 장면을 실제로 밝게 비추기 위해서는 광원 색상을 (4,4,4)와 같이
+ * 고강도로 설정하는 것이 필요합니다.
  */
